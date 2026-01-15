6c77ec7bb75526ae5aee143eabb6db3a135ec20b
glutesha <mail@glutesha.me>
2026-01-15 15:23:37 +0300

    Add buffer to cache previous changes and improve accuracy

--- src/modbus/modbus.c
+++ src/modbus/modbus.c
@@ -78,42 +78,72 @@ esp_err_t mb_master_init(void) {
 }
 
 void modbus_read_master_task(void *arg) {
+  double ring[MB_RING_SIZE][3];
+  bool init = false;
+
+  for (int i = 0; i < MB_RING_SIZE; i++) {
+    for (int b = 0; b < 3; b++) {
+      ring[i][b] = 0;
+    }
+  }
+
   while (1) {
-    bool get_correct = false;
-    double vals[3] = {0, 0, 0};
+    for (int i = 0; i < MB_RING_SIZE; i++) {
+      bool get_correct = false;
+      double vals[3] = {0, 0, 0};
 
-    for (uint16_t cid = 0; cid < 3; cid++) {
-      const mb_parameter_descriptor_t *param_info = NULL;
-      esp_err_t err = mbc_master_get_cid_info(mbc_ctx, cid, &param_info);
-      if (err == ESP_OK && param_info != NULL) {
-        uint8_t data_buf[8];
-        uint8_t type = 0;
-        err = mbc_master_get_parameter(mbc_ctx, param_info->cid, data_buf, &type);
-        if (err == ESP_OK) {
-          if (!get_correct) {get_correct = true;}
-          uint32_t raw = 0;
-          memcpy(&raw, data_buf, param_info->param_size);
-          uint16_t value = *(uint16_t*)data_buf;
-          double correct = value;
+      for (int cid = 0; cid < 3; cid++) {
+        const mb_parameter_descriptor_t *param_info = NULL;
+        esp_err_t err = mbc_master_get_cid_info(mbc_ctx, cid, &param_info);
+        if (err == ESP_OK && param_info != NULL) {
+          int data_buf[8];
+          int type = 0;
+          err = mbc_master_get_parameter(mbc_ctx, param_info->cid, data_buf, &type);
+          if (err == ESP_OK) {
+            if (!get_correct) {get_correct = true;}
+            uint32_t raw = 0;
+            memcpy(&raw, data_buf, param_info->param_size);
+            int value = *(uint16_t*)data_buf;
+            double correct = value;
 
-          if (cid == 0) correct /= 10.0;
-          if (cid == 1) correct /= 100.0;
+            if (cid == 0) correct /= 10.0;
+            if (cid == 1) correct /= 100.0;
 
-          vals[cid] = correct;
-          ESP_LOGI(TAG, "CID %d %s = %f %s", cid, param_info->param_key, correct, param_info->param_units);
+            vals[cid] = correct;
+            ESP_LOGI(TAG, "CID %d %s = %f %s", cid, param_info->param_key, correct, param_info->param_units);
 
+          } else {
+            if (get_correct) {get_correct = false;}
+            ESP_LOGE(TAG, "Read CID %d error: 0x%x", cid, err);
+          }
         } else {
-          if (get_correct) {get_correct = false;}
-          ESP_LOGE(TAG, "Read CID %d error: 0x%x", cid, err);
+          ESP_LOGE(TAG, "CID info %d not found: 0x%x", cid, err);
         }
-      } else {
-        ESP_LOGE(TAG, "CID info %d not found: 0x%x", cid, err);
       }
-    }
 
-    if (get_correct) {
-      send_request(vals[0], vals[1], vals[2]);
+      if (get_correct) {
+        for (int b = 0; b < 3; b++) {
+          ring[i][b] = vals[b];
+        }
+        if (i == MB_RING_SIZE - 1) {
+          init = true;
+        }
+        if (init) {
+          double correct_vals[3] = {0, 0, 0};
+          for (int b = 0; b < MB_RING_SIZE; b++) {
+            for (int c = 0; c < 3; c++) {
+              correct_vals[c] += ring[b][c];
+            }
+          }
+
+          for (int c = 0; c < 3; c++) {
+            correct_vals[c] /= MB_RING_SIZE;
+          }
+
+          send_request(correct_vals[0], correct_vals[1], correct_vals[2]);
+        }
+      }
+      vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
